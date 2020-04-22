e0f579c6927b838aa7b5d14a8d2b8989d3a2dd75
liuchaochen <liuchaochen@blacklake.cn>
2020-04-22 17:30:01 +0800

    两数之和

---
+++ 两数之和.kt
@@ -0,0 +1,41 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val num1s = nums.filter { 1 and it == 1  }
+        val num2s = nums.filter { 1 and it == 0  }
+        if (1 and target ==1){
+            num1s.forEach {num1->
+                val num2 = num2s.filter { it == target - num1 }
+                if (num2.isNotEmpty()) return intArrayOf(nums.indexOf(num1),nums.indexOf(num2.first()))
+            }
+            return intArrayOf()
+        }else{
+            num1s.forEachIndexed { indexNum, num ->
+                val numO = num1s.filterIndexed { index, i ->
+                    indexNum != index && i == target - num
+                }
+
+                if (numO.isNotEmpty()) {
+                    if (numO.first()== num){
+                        return intArrayOf(nums.indexOf(num),nums.lastIndexOf(numO.first()))
+                    }else{
+                        return intArrayOf(nums.indexOf(num),nums.indexOf(numO.first()))
+                    }
+                }
+
+            }
+            num2s.forEachIndexed { indexNum, num ->
+                val numO = num2s.filterIndexed { index, i ->
+                    indexNum != index && i == target - num
+                }
+                if (numO.isNotEmpty()) {
+                    if (numO.first()== num){
+                        return intArrayOf(nums.indexOf(num),nums.lastIndexOf(numO.first()))
+                    }else{
+                        return intArrayOf(nums.indexOf(num),nums.indexOf(numO.first()))
+                    }
+                }
+            }
+            return intArrayOf()
+        }
+    }
+}
